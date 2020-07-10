a5d62806347d31828f204fc61d302a1bd165ec19
Noé <35569564+NoeCR@users.noreply.github.com>
2020-07-10 19:33:56 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   apiUrl: 'http://localhost:5000/firestore-chart-3380f/us-central1',
   firebase: {
-    apiKey: 'AIzaSyAOzrzB8cLy7IO2hG3LDpLSATALcpVEjYg',
-    authDomain: 'firestore-chart-3380f.firebaseapp.com',
-    databaseURL: 'https://firestore-chart-3380f.firebaseio.com',
-    projectId: 'firestore-chart-3380f',
-    storageBucket: 'firestore-chart-3380f.appspot.com',
-    messagingSenderId: '104205113941',
-    appId: '1:104205113941:web:705769bf65c3b1138cd2dc'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
   }
 };
