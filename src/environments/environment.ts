6d58c29cf6961b58680d43080e564a6b26d7304c
Noé <35569564+NoeCR@users.noreply.github.com>
2020-07-10 19:33:23 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@ export const environment = {
   production: false,
   apiUrl: 'https://us-central1-firestore-chart-3380f.cloudfunctions.net',
   firebase: {
-    apiKey: "AIzaSyAOzrzB8cLy7IO2hG3LDpLSATALcpVEjYg",
-    authDomain: "firestore-chart-3380f.firebaseapp.com",
-    databaseURL: "https://firestore-chart-3380f.firebaseio.com",
-    projectId: "firestore-chart-3380f",
-    storageBucket: "firestore-chart-3380f.appspot.com",
-    messagingSenderId: "104205113941",
-    appId: "1:104205113941:web:705769bf65c3b1138cd2dc"
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
   }
 };
 
